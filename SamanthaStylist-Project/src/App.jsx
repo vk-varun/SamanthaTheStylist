00723ad2688cba60884a37c9a94e299bf221c8e4
vk-varun <97547208+vk-varun@users.noreply.github.com>
2026-02-03 13:58:55 -0700

    Initial Vite + React project scaffold

---
+++ SamanthaStylist-Project/src/App.jsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import "./App.css";
+import Nav from "./Nav/Nav.jsx";
+import Home from "./Home/Home.jsx";
+import About from "./About/About.jsx";
+import Services from "./Services/Services.jsx";
+import Booking from "./Booking/Booking.jsx";
+import Gallery from "./Gallery/Gallery.jsx";
+import Footer from "./Footer/Footer.jsx";
+
+function App() {
+  const [selectedServices, setSelectedServices] = useState([]);
+
+  return (
+    <div className="appRoot">
+      <div className="appInner">
+        <Nav />
+        <main>
+          <section id="home">
+            <Home />
+          </section>
+          <section id="about">
+            <About />
+          </section>
+          {/* <section id="services">
+            <Services />
+          </section>
+          <section id="booking">
+            <Booking />
+          </section> */}
+          <section id="services">
+            <Services onSelectService={setSelectedServices} />
+          </section>
+          <section id="booking">
+            <Booking selectedServices={selectedServices} />
+          </section>
+          <section id="gallery">
+            <Gallery />
+          </section>
+        </main>
+        <Footer />
+      </div>
+    </div>
+  );
+}
+
+export default App;
